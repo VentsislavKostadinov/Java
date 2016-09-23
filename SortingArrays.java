d8cf1fceac17bfa8cf11ce9b0d7f7a20c7e26d15
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2016-09-23 15:52:05 +0300

    Update SortingArrays.java

--- SortingArrays.java
+++ SortingArrays.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class SortingArrays {
 	
-	public static void main(String[] args) {
+	
+	public static void sortArrays() {
 		
-		int[] numbers = { 5, 3, 8, 7, 10, 100, 21, 50, 1001};
+int[] numbers = { 5, 3, 8, 7, 10, 100, 21, 50, 1001};
 		
 		Arrays.sort(numbers);
 		
@@ -15,6 +16,12 @@ public class SortingArrays {
         	 System.out.printf(" Numbers: %d ", i);
        }
 		
-     	}
+	}
 	
+	public static void main(String[] args) {
+		
+		sortArrays();
+		
+    }
+
 }
